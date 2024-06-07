7dc66df47084c6acd8e180758c24019e9d732a8b
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-06-07 15:16:12 +0000

    Fix rebuild issue

--- src/utils/checkKeys.js
+++ src/utils/checkKeys.js
@@ -1,10 +1,15 @@
-import { Configuration, OpenAIApi } from 'openai';
+//import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 export const checkApiKey = async (keys) => {
-  const configuration = new Configuration({
+  /*const configuration = new Configuration({
     apiKey: keys,
-  });
+  });*/
 
-  const openai = new OpenAIApi(configuration);
+  //const openai = new OpenAIApi(configuration);
+
+  const openai = new OpenAI({
+    apiKey: keys, // This is the default and can be omitted
+  });
 
   return openai.listModels();
 };
--- src/utils/dalle.js
+++ src/utils/dalle.js
@@ -1,12 +1,16 @@
-import { Configuration, OpenAIApi } from 'openai';
+//import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 export const dalle = async (prompt, key) => {
-  const configuration = new Configuration({
+  /*const configuration = new Configuration({
     apiKey: key,
-  });
+  });*/
 
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createImage({
+  //const openai = new OpenAIApi(configuration);
+  const openai = new OpenAI({
+    apiKey: key, // This is the default and can be omitted
+  });
+  const response = await openai.images.generate({
     prompt: `${prompt}`,
     n: 1,
     size: '512x512',
